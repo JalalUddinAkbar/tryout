e2f797c4c70ca3e7b13839a8628aecc7860d962b
JalalUddinAkbar <nisatctg56@gmail.com>
2021-07-10 22:28:14 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import First from './components/First/First';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>This is app.js</h1>
+
+      <First></First>
+    </div>
+  );
+}
+
+export default App;
